fc64cb8d81f8a19bab4da1af67c44f84d40506ae
SKPnation <ayomideseaz@gmail.com>
2025-10-13 15:39:02 -0500

    git added version

--- lib/features/onboarding/presentation/pages/user_type_page.dart
+++ lib/features/onboarding/presentation/pages/user_type_page.dart
@@ -15,64 +15,72 @@ class UserTypePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(ImageElements.pvamuLogo, height: 200, width: 200),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(ImageElements.pvamuLogo, height: 200, width: 200),
 
-            const SizedBox(height: 15),
+                const SizedBox(height: 15),
 
-            CustomText(
-              text: AppStrings.slogan,
-              color: AppColors.grey[300],
-              size: 22,
-              fontStyle: FontStyle.italic,
-              weight: FontWeight.bold,
-            ),
+                CustomText(
+                  text: AppStrings.slogan,
+                  color: AppColors.grey[300],
+                  size: 22,
+                  fontStyle: FontStyle.italic,
+                  weight: FontWeight.bold,
+                ),
 
-            const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-            ShaderMask(
-              shaderCallback:
-                  (bounds) => LinearGradient(
-                colors: [
-                  AppColors.gold, // Gold
-                  AppColors.purple,
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ).createShader(
-                Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-              ),
-              child: CustomText(
-                text: AppStrings.appTitle,
-                color: Colors.white,
-                // Important: must be set, even if overridden
-                size: 28,
-                weight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 60),
+                ShaderMask(
+                  shaderCallback:
+                      (bounds) => LinearGradient(
+                    colors: [
+                      AppColors.gold, // Gold
+                      AppColors.purple,
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ).createShader(
+                    Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                  ),
+                  child: CustomText(
+                    text: AppStrings.appTitle,
+                    color: Colors.white,
+                    // Important: must be set, even if overridden
+                    size: 28,
+                    weight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 60),
 
-            SizedBox(
-                width: 300,
-                child: Column(
-                  children: [
-                    CustomText(text: "Select your role type", size: AppFonts.baseSize),
-                    const SizedBox(height: 16),
-                    CustomButton(onPressed: ()=>onboardingController.chooseTutor(), text: "Tutor", bgColor: AppColors.purple),
-                    SizedBox(height: 8),
-                    CustomButton(onPressed: ()=>onboardingController.chooseStudent(), text: "Student", bgColor: AppColors.gold),
-                    SizedBox(height: 8),
-                  ],
+                SizedBox(
+                    width: 300,
+                    child: Column(
+                      children: [
+                        CustomText(text: "Select your role type", size: AppFonts.baseSize),
+                        const SizedBox(height: 16),
+                        CustomButton(onPressed: ()=>onboardingController.chooseTutor(), text: "Tutor", bgColor: AppColors.purple),
+                        SizedBox(height: 8),
+                        CustomButton(onPressed: ()=>onboardingController.chooseStudent(), text: "Student", bgColor: AppColors.gold),
+                        SizedBox(height: 8),
+                      ],
+                    )
                 )
-            )
 
 
-          ],
-        ),
+              ],
+            ),
+          ),
 
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: CustomText(text: "1.0.1", color: AppColors.purpleDarker),
+          ),
+        ],
       )
     );
   }
