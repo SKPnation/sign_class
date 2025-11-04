a1f6b36656317dd90748537834861d38f1442cad
SKPnation <ayomideseaz@gmail.com>
2025-11-04 09:23:19 -0600

    added 'Developed by Ayomide Ajayi & Dr Emmanuel U. Nzewi'

--- lib/features/user_types.dart
+++ lib/features/user_types.dart
@@ -15,73 +15,97 @@ class UserTypesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(ImageElements.pvamuLogo, height: 200, width: 200),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(ImageElements.pvamuLogo, height: 200, width: 200),
 
-                  const SizedBox(height: 15),
+                const SizedBox(height: 15),
 
-                  CustomText(
-                    text: AppStrings.slogan,
-                    color: AppColors.grey[300],
-                    size: 22,
-                    fontStyle: FontStyle.italic,
-                    weight: FontWeight.bold,
-                  ),
+                CustomText(
+                  text: AppStrings.slogan,
+                  color: AppColors.grey[300],
+                  size: 22,
+                  fontStyle: FontStyle.italic,
+                  weight: FontWeight.bold,
+                ),
 
-                  const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-                  ShaderMask(
-                    shaderCallback:
-                        (bounds) => LinearGradient(
-                      colors: [
-                        AppColors.gold, // Gold
-                        AppColors.purple,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ).createShader(
-                      Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-                    ),
-                    child: CustomText(
-                      text: AppStrings.appTitle,
-                      color: Colors.white,
-                      // Important: must be set, even if overridden
-                      size: 28,
-                      weight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 60),
-
-                  SizedBox(
-                      width: 300,
-                      child: Column(
-                        children: [
-                          CustomText(text: "Select your role type", size: AppFonts.baseSize),
-                          const SizedBox(height: 16),
-                          CustomButton(onPressed: ()=>onboardingController.chooseTutor(), text: "Tutor", bgColor: AppColors.purple),
-                          SizedBox(height: 8),
-                          CustomButton(onPressed: ()=>onboardingController.chooseStudent(), text: "Student", bgColor: AppColors.gold),
-                          SizedBox(height: 8),
+                ShaderMask(
+                  shaderCallback:
+                      (bounds) => LinearGradient(
+                        colors: [
+                          AppColors.gold, // Gold
+                          AppColors.purple,
                         ],
-                      )
-                  )
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ).createShader(
+                        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                      ),
+                  child: CustomText(
+                    text: AppStrings.appTitle,
+                    color: Colors.white,
+                    // Important: must be set, even if overridden
+                    size: 28,
+                    weight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 60),
 
+                SizedBox(
+                  width: 300,
+                  child: Column(
+                    children: [
+                      CustomText(
+                        text: "Select your role type",
+                        size: AppFonts.baseSize,
+                      ),
+                      const SizedBox(height: 16),
+                      CustomButton(
+                        onPressed: () => onboardingController.chooseTutor(),
+                        text: "Tutor",
+                        bgColor: AppColors.purple,
+                      ),
+                      SizedBox(height: 8),
+                      CustomButton(
+                        onPressed: () => onboardingController.chooseStudent(),
+                        text: "Student",
+                        bgColor: AppColors.gold,
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
 
-                ],
+          Positioned(
+            // alignment: Alignment.bottomCenter,
+            bottom: 40,
+            left: 0,
+            right: 0,
+
+            child: Center(
+              child: CustomText(
+                text: "Developed by Ayomide Ajayi & Dr Emmanuel U. Nzewi",
+                color: AppColors.grey[300],
               ),
             ),
+          ),
 
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: CustomText(text: "1.0.0+3", color: AppColors.purpleDarker),
-            ),
-          ],
-        )
+
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: CustomText(text: "1.0.0+3", color: AppColors.grey[200]),
+          ),
+        ],
+      ),
     );
   }
 }
